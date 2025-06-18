6a51a717f4c732c60cd61a4684b5bf14d5247e83
Nicolas Maureira <ni.maureira@duocuc.cl>
2025-06-18 19:39:58 -0400

    registro

---
+++ REGISTRARSE/Registro_Usuario.php
@@ -0,0 +1,27 @@
+<!DOCTYPE htm>
+<html lang="en">
+<head>
+    <meta charset ="UTF-8">
+    <meta http-equiuv="X-UA.Compatible" content ="IE=edge">
+    <meta name=viewport content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/basic.css" rel ="stylesheet" integrity ="sha384-1BmE4kWBq78iYhFld6x8j6b6d">
+    <link rel= "stylesheet" href="Style.css">
+    <tittle> Registrarse </tittle>
+
+</head>
+<body>
+    <div class="contenedor">
+        <h1> <ins>Registrarse</ins></h1>
+        <br>
+        <form action="">
+            <label for="">
+            <i class="fa-solid fa-user" ></i>
+                Usuario
+            </label>
+            <input type ="text" placeholder="Ingrese usuario" name="Usuario">
+            <label for=""></label>
+
+            </label>
+        </form>
+    </div>
+</body>
