bc104bb26ded0666ede12187d7d869934f3530b9
Nicolas Maureira <ni.maureira@duocuc.cl>
2025-06-19 22:55:51 -0400

    User user

--- Conexion.php
+++ Conexion.php
@@ -7,7 +7,7 @@
     $db= "db_sistema_aduanas";
 
 
-    $conexion = mysqli_connect($host, $User,$pass, $db);
+    $conexion = mysqli_connect($host, $user,$pass, $db);
     
     if(!$con){
     echo "Conexion fallida";
