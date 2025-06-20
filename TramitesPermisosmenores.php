2045321e595541f40763ee3ed6a0ce11bdd47437
Tomas <to.castaneda@duocuc.cl>
2025-06-19 22:50:54 -0400

    TramitesPermisosMenores

---
+++ TramitesPermisosmenores.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang ="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+        <meta name ="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3-a/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link reñ= "stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/frony-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/UG6swU2Im1vVX05Vk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <title>Tramites y Permisos De Menores</title>
+       /* estilo para el body, contenedor, titulo y subtitulo, parrafos y lista de tramites y permisos de menores */
+       <style>
+            body {
+            background-color: black;
+            color: white;
+            font-family: Arial, sans-serif;
+            }
+            /* sirve para que el contenedor tenga un max-width de 800px, se centre en la pantalla y tenga un padding de 20px */
+            .container{
+                max-width: 800px;
+                margin: auto;
+                padding: 20px;
+                background-color: #333;
+                border-radius: 10px;
+            }
+            /*sirve para que el titulo y subtitulo se vean centrados y con un color blanco*/
+            h1, h2 {
+                text-align: center;
+                color: black;
+            }
+            /*sirve para que el titulo tenga un tamaño de 2.5rem y el subtitulo un tamaño de 1.5rem */
+            p{
+                text-align: justify;
+                line-height: 1.6;
+                color: white;
+            }
+            /* Estilo para la lista de tramites y permisoso */
+            .list-group-item{
+                background-color: whitesmoke;
+                color:black;
+                border: 1px solid #ccc;
+                border-radius: 10px;
+                margin-bottom: 10px;
+            }
+            /* Sirve para que cuando pases el mouse por encima del item de la lista, cambie de color */
+            .list-group-item.hover:hover {
+                background-color: #333;
+                color: whitesmoke;
+            }
+            /*¨sirve para que el link dentro del item de la lista, cambie de color al pasar el mouse por encima */
+            .list-group-item a {
+                color: black;
+                text-decoration: none;
+            }
+            /*sirve para que el link dentro del item de la lista, cambie de color al pasar el mouse por encima */
+            .list-group-item a:hover {
+                color: white;
+                text-decoration: underline;
+            }
+            
+
+        </style>
+        </head>
+
+    
